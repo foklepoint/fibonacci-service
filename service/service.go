d6632e67df89d70ea954ffb9cdbb65d24b755521
Saurabh Sharma <live.mellon@gmail.com>
2017-04-05 10:25:00 -0700

    Make cache multi-thread access safe

--- service/service.go
+++ service/service.go
@@ -1,7 +1,10 @@
 // Defines the fibonacci microservice
 package service
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // Fibonacci service interface
 type IFibonacciService interface {
@@ -12,6 +15,7 @@ type IFibonacciService interface {
 // Keeps a cache of calculated values
 type FibonacciService struct {
 	cache map[uint64]uint64
+	mux   sync.Mutex
 }
 
 // Calculates nth fibonacci number, nth >= 1
@@ -22,17 +26,27 @@ func (service *FibonacciService) calculate(n uint64) (ret uint64, err error) {
 		return
 	}
 	if service.cache == nil {
+		service.mux.Lock()
 		service.cache = make(map[uint64]uint64)
+		service.mux.Unlock()
 	}
 	if n == 1 || n == 2 {
 		ret = 1
-	} else if val, ok := service.cache[n]; ok {
-		ret = val
-	} else {
-		n_2, _ := service.calculate(n - 2)
-		n_1, _ := service.calculate(n - 1)
-		ret = n_1 + n_2
+		return
 	}
+	service.mux.Lock()
+	if val, ok := service.cache[n]; ok {
+		service.mux.Unlock()
+		ret = val
+		return
+	}
+	service.mux.Unlock()
+
+	n_2, _ := service.calculate(n - 2)
+	n_1, _ := service.calculate(n - 1)
+	ret = n_1 + n_2
+	service.mux.Lock()
 	service.cache[n] = ret
+	service.mux.Unlock()
 	return
 }
