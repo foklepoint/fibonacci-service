e700981844d19f8088c64e3353cae232742afb63
Saurabh Sharma <live.mellon@gmail.com>
2017-04-05 10:26:27 -0700

    general clean up

--- service/logging.go
+++ service/logging.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"fmt"
 	"github.com/go-kit/kit/log"
 	"time"
 )
@@ -15,6 +14,5 @@ func (mw LoggingMiddleware) calculate(n uint64) (uint64, error) {
 	defer func(begin time.Time) {
 		mw.Logger.Log("method", "calculate", "n", n, "time", time.Since(begin))
 	}(time.Now())
-	fmt.Println(len(mw.Next.cache))
 	return mw.Next.calculate(n)
 }
